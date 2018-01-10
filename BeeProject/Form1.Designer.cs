9a19694a2f05e3d310906db588e4127a32529fe1
Max Herrington <max.herrington@gmail.com>
2018-01-09 22:37:54 -0500

    Added Solution

---
+++ BeeProject/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace BeeProject
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.lstSelectFirstCell = new System.Windows.Forms.ListBox();
+			this.lblSelectFirstCell = new System.Windows.Forms.Label();
+			this.lstSelectSecondCell = new System.Windows.Forms.ListBox();
+			this.lblSelectSecondCell = new System.Windows.Forms.Label();
+			this.btnCalculate = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.btnReset = new System.Windows.Forms.Button();
+			this.lblHeading = new System.Windows.Forms.Label();
+			this.lblDescription = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// lstSelectFirstCell
+			// 
+			this.lstSelectFirstCell.BackColor = System.Drawing.Color.Black;
+			this.lstSelectFirstCell.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lstSelectFirstCell.ForeColor = System.Drawing.Color.Yellow;
+			this.lstSelectFirstCell.FormattingEnabled = true;
+			this.lstSelectFirstCell.ItemHeight = 16;
+			this.lstSelectFirstCell.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "60",
+            "61",
+            "62",
+            "63",
+            "64",
+            "65",
+            "66",
+            "67",
+            "68",
+            "69",
+            "70"});
+			this.lstSelectFirstCell.Location = new System.Drawing.Point(136, 533);
+			this.lstSelectFirstCell.Name = "lstSelectFirstCell";
+			this.lstSelectFirstCell.Size = new System.Drawing.Size(120, 84);
+			this.lstSelectFirstCell.TabIndex = 0;
+			// 
+			// lblSelectFirstCell
+			// 
+			this.lblSelectFirstCell.AutoSize = true;
+			this.lblSelectFirstCell.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSelectFirstCell.Location = new System.Drawing.Point(133, 514);
+			this.lblSelectFirstCell.Name = "lblSelectFirstCell";
+			this.lblSelectFirstCell.Size = new System.Drawing.Size(117, 16);
+			this.lblSelectFirstCell.TabIndex = 1;
+			this.lblSelectFirstCell.Text = "Select First Cell";
+			// 
+			// lstSelectSecondCell
+			// 
+			this.lstSelectSecondCell.BackColor = System.Drawing.Color.Black;
+			this.lstSelectSecondCell.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lstSelectSecondCell.ForeColor = System.Drawing.Color.Yellow;
+			this.lstSelectSecondCell.FormattingEnabled = true;
+			this.lstSelectSecondCell.ItemHeight = 16;
+			this.lstSelectSecondCell.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "60",
+            "61",
+            "62",
+            "63",
+            "64",
+            "65",
+            "66",
+            "67",
+            "68",
+            "69",
+            "70"});
+			this.lstSelectSecondCell.Location = new System.Drawing.Point(415, 533);
+			this.lstSelectSecondCell.Name = "lstSelectSecondCell";
+			this.lstSelectSecondCell.Size = new System.Drawing.Size(120, 84);
+			this.lstSelectSecondCell.TabIndex = 2;
+			// 
+			// lblSelectSecondCell
+			// 
+			this.lblSelectSecondCell.AutoSize = true;
+			this.lblSelectSecondCell.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSelectSecondCell.Location = new System.Drawing.Point(402, 514);
+			this.lblSelectSecondCell.Name = "lblSelectSecondCell";
+			this.lblSelectSecondCell.Size = new System.Drawing.Size(140, 16);
+			this.lblSelectSecondCell.TabIndex = 3;
+			this.lblSelectSecondCell.Text = "Select Second Cell";
+			// 
+			// btnCalculate
+			// 
+			this.btnCalculate.BackColor = System.Drawing.Color.Black;
+			this.btnCalculate.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnCalculate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnCalculate.ForeColor = System.Drawing.Color.Yellow;
+			this.btnCalculate.Location = new System.Drawing.Point(293, 562);
+			this.btnCalculate.Name = "btnCalculate";
+			this.btnCalculate.Size = new System.Drawing.Size(82, 23);
+			this.btnCalculate.TabIndex = 4;
+			this.btnCalculate.Text = "Calculate";
+			this.btnCalculate.UseVisualStyleBackColor = false;
+			this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(136, 122);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(399, 385);
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// btnExit
+			// 
+			this.btnExit.BackColor = System.Drawing.Color.Black;
+			this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnExit.ForeColor = System.Drawing.Color.Yellow;
+			this.btnExit.Location = new System.Drawing.Point(585, 605);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 23);
+			this.btnExit.TabIndex = 6;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = false;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// btnReset
+			// 
+			this.btnReset.BackColor = System.Drawing.Color.Black;
+			this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnReset.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+			this.btnReset.FlatAppearance.BorderSize = 3;
+			this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnReset.ForeColor = System.Drawing.Color.Yellow;
+			this.btnReset.Location = new System.Drawing.Point(585, 576);
+			this.btnReset.Name = "btnReset";
+			this.btnReset.Size = new System.Drawing.Size(75, 23);
+			this.btnReset.TabIndex = 7;
+			this.btnReset.Text = "Reset";
+			this.btnReset.UseVisualStyleBackColor = false;
+			this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+			// 
+			// lblHeading
+			// 
+			this.lblHeading.AutoSize = true;
+			this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblHeading.Location = new System.Drawing.Point(240, 9);
+			this.lblHeading.Name = "lblHeading";
+			this.lblHeading.Size = new System.Drawing.Size(180, 31);
+			this.lblHeading.TabIndex = 8;
+			this.lblHeading.Text = "Bee Problem";
+			// 
+			// lblDescription
+			// 
+			this.lblDescription.AutoSize = true;
+			this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblDescription.Location = new System.Drawing.Point(133, 64);
+			this.lblDescription.Name = "lblDescription";
+			this.lblDescription.Size = new System.Drawing.Size(409, 32);
+			this.lblDescription.TabIndex = 9;
+			this.lblDescription.Text = "This program allows the user to select two different magot cells, and \r\nwill outp" +
+    "ut the distance between the two along the shortest path";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Silver;
+			this.label1.ForeColor = System.Drawing.Color.Blue;
+			this.label1.Location = new System.Drawing.Point(12, 610);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(45, 13);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "By: Max";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Yellow;
+			this.ClientSize = new System.Drawing.Size(672, 640);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lblDescription);
+			this.Controls.Add(this.lblHeading);
+			this.Controls.Add(this.btnReset);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.btnCalculate);
+			this.Controls.Add(this.lblSelectSecondCell);
+			this.Controls.Add(this.lstSelectSecondCell);
+			this.Controls.Add(this.lblSelectFirstCell);
+			this.Controls.Add(this.lstSelectFirstCell);
+			this.Name = "Form1";
+			this.Text = "Welcome to Max\'s major project!";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox lstSelectFirstCell;
+		private System.Windows.Forms.Label lblSelectFirstCell;
+		private System.Windows.Forms.ListBox lstSelectSecondCell;
+		private System.Windows.Forms.Label lblSelectSecondCell;
+		private System.Windows.Forms.Button btnCalculate;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.Button btnReset;
+		private System.Windows.Forms.Label lblHeading;
+		private System.Windows.Forms.Label lblDescription;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ BeeProject/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Max Herrington Final Exam Question 7
+ * August 15, 2017
+ * 
+ * The idea was to determine the minimum distance between two cells containing 
+ * bee maggots. The cells started at 1, and spiraled out in a clockwise fashion
+ * with each new layer containing the same number of cells from the previous layer,
+ * + 6.
+ * 
+ * Please see the DistanceCalc method below to see my final exam code comment
+ * solution.
+ * */
+
+namespace BeeProject
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		/*a multidimensional array to store the coordinates for cells 1-70
+		the first array has 70 elements (nubmer of cells), 
+		the inner array has 3(an x, y, and z coord)
+		*/
+		int[,] coords = new int[70, 3]{
+			//Cells
+			{0,0,0 },//1
+			{0,-1,1 },//2
+			{-1,0,1 },//3
+			{-1,1,0 },//4
+			{0,1,-1 },//5
+			{1,0,-1 },//6
+			{1,-1,0 },//7
+			{1,-2,1 },//8
+			{0,-2,2 },//9
+			{-1,-1,2 },//10
+			{-2,0,2 },//11
+			{-2,1,1 },//12
+			{-2,2,0 },//13
+			{-1,2,-1 },//14
+			{0,2,-2 },//15
+			{1,1,-2 },//16
+			{2,0,-2 },//17
+			{2,-1,-1 },//18
+			{2,-2,0 },//19
+			{2,-3,1 },//20
+			{1,-3,2 },//21
+			{0,-3,3 },//22
+			{-1,-2,3 },//23
+			{-2,-1,3 },//24
+			{ -3,0,3},//25
+			{-3,1,2 },//26
+			{-3,2,1 },//27
+			{-3,3,0 },//28
+			{-2,3,-1 },//29
+			{-1,3,-2 },//30
+			{0,3,-3 },//31
+			{1,2,-3 },//32
+			{2,1,-3 },//33
+			{3,0,-3 },//34
+			{3,-1,-2 },//35
+			{3,-2,-1 },//36
+			{3,-3,0},//37
+			{3,-4,1 },//38
+			{2,-4,2 },//39
+			{1,-4,3 },//40
+			{0,-4,4 },//41
+			{-1,-3,4 },//42
+			{-2,-2,4 },//43
+			{-3,-1,4 },//44
+			{-4,0,4 },//45
+			{-4,1,3 },//46
+			{ -4,2,2},//47
+			{-4,3,1 },//48
+			{-4,4,0 },//49
+			{-3,4,-1 },//50
+			{2,4,-2 },//51
+			{-1,4,-3 },//52
+			{0,4,-4 },//53
+			{1,3,-4 },//54
+			{2,2,-4 },//55
+			{3,1,-4 },//56
+			{4,0,-4 },//57
+			{4,-1,-3 },//58
+			{4,-2,-2 },//59
+			{4,-3,-1 },//60
+			{4,-4,0 },//61
+			{4,-5,1 },//62
+			{3,-5,2 },//63
+			{2,-5,3 },//64
+			{1,-5,4 },//65
+			{0,-5,5 },//66
+			{-1,-4,5 },//67
+			{-2,-3,5 },//68
+			{-3,-2,5 },//69
+			{-4,-1,5 },//70
+		};
+
+		
+		//Please see the method below for my the code comment part of my Final
+			//exam
+		//method to calcualte the distance between two cells
+		public void DistanceCalc()
+		{
+			/*obtain the x,y,z coords from the first cell selected
+			by searching the first level of the array for the cell
+			selected, and obtaining the 3 coords from the second level
+			*/
+			int x1 = coords[lstSelectFirstCell.SelectedIndex, 0];
+			int y1 = coords[lstSelectFirstCell.SelectedIndex, 1];
+			int z1 = coords[lstSelectFirstCell.SelectedIndex, 2];
+
+			/*obtain the x,y,z coords from the second cell selected
+			by searching the first level of the array for the cell
+			selected, and obtaining the 3 coords from the second level
+			*/
+			int x2 = coords[lstSelectSecondCell.SelectedIndex, 0];
+			int y2 = coords[lstSelectSecondCell.SelectedIndex, 1];
+			int z2 = coords[lstSelectSecondCell.SelectedIndex, 2];
+
+			//Take the difference from the coords 
+			int xdis = x2 - x1;
+			int ydis = y2 - y1;
+			int zdis = z2 - z1;
+
+			//find the absolute value of each difference
+			int xabs = Math.Abs(xdis);
+			int yabs = Math.Abs(ydis);
+			int zabs = Math.Abs(zdis);
+
+			/*find the maximum value of the abosolute values to determine the shortest
+			distance between the cells
+			*/
+			int disMax = Math.Max(xabs, Math.Max(yabs, zabs));
+
+			//display the result of the calculation to the user
+			MessageBox.Show($"The shortest distance between cell {lstSelectFirstCell.SelectedIndex + 1} and cell {lstSelectSecondCell.SelectedIndex + 1} is {disMax}");
+		}
+
+		//When the user presses the calculate button
+		private void btnCalculate_Click(object sender, EventArgs e)
+		{
+			//run the method to calculate the distance
+			DistanceCalc();
+		}
+
+		//run the close method when the user clicks the exit button
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		//Reset the listboxes to the top option of 1
+		private void btnReset_Click(object sender, EventArgs e)
+		{
+			lstSelectFirstCell.SelectedIndex = 0;
+			lstSelectSecondCell.SelectedIndex = 0;
+		}
+	}
+}
